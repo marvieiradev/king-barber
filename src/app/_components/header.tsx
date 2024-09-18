5ba0068fb32f2498b685acd2eb57fa5620403dda
Marcos Vieira <marcosvieira482@gmail.com>
2024-09-18 00:04:06 -0300

    feat: add quick search

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <Card>
+    <Card className="rounded-t-none">
       <CardContent className="p-5 flex flex-row justify-between items-center">
-        <Image src="/logo.png" height={18} width={120} alt="logo" />
+        <Image src="/logo.png" height={20} width={130} alt="logo" />
         <Button size="icon" variant="outline">
           <MenuIcon />
         </Button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,9 @@ import BarbershopItem from "./_components/barbershop-item";
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({});
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: { name: "desc" },
+  });
   return (
     <div>
       <Header />
@@ -23,12 +26,60 @@ const Home = async () => {
             <SearchIcon />
           </Button>
         </div>
+
+        <div className="flex gap-3 mt-6 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <Button className="gap-2" variant="secondary">
+            <Image src="/cabelo.svg" width={16} height={16} alt="cabelo" />
+            Cabelo
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image src="/barba.svg" width={16} height={16} alt="barba" />
+            Barba
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image
+              src="/acabamento.svg"
+              width={16}
+              height={16}
+              alt="acabamento"
+            />
+            Acabamento
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image
+              src="/hidratacao.svg"
+              width={16}
+              height={16}
+              alt="hidratação"
+            />
+            Hidratação
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image
+              src="/sobrancelha.svg"
+              width={16}
+              height={16}
+              alt="sobrancelha"
+            />
+            Sobrancelha
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image src="/massagem.svg" width={16} height={16} alt="massagem" />
+            Massagem
+          </Button>
+        </div>
+
         <div className="relative w-full h-[150px] mt-6">
           <Image
             alt="banner"
-            src="/banner01.png"
+            src="/banner-01.png"
             fill
-            className="object-contain rounded-xl"
+            className="object-cover rounded-xl"
           />
         </div>
 
@@ -64,7 +115,26 @@ const Home = async () => {
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        <h2 className="mt-6 mb-3 uppercase text-xs font-bold text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+
+      <footer>
+        <Card className="rounded-none">
+          <CardContent className="py-5 flex justify-center">
+            <p className="text-sm text-gray-400">
+              &copy; 2024 - <span className="font-bold">King Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   );
 };
