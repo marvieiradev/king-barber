f3dddb7c9629515eecd796036f5c0bb8617d4cab
Marcos Vieira <marcosvieira482@gmail.com>
2024-09-19 12:32:27 -0300

    chore: add next-auth setup

--- src/app/_components/sidebar-sheet.tsx
+++ src/app/_components/sidebar-sheet.tsx
@@ -1,10 +1,16 @@
-import { CalendarIcon, HomeIcon, LogInIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
-import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import { quickSearchOptions } from "../_constants/search";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const SidebarSheet = () => {
   return (
@@ -12,7 +18,28 @@ const SidebarSheet = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
-      <div className="py-5 flex items-center border-b border-solid gap-3">
+
+      <div className="py-5 flex items-center border-b border-solid gap-3 justify-between">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%] flex flex-col items-center">
+            <DialogTitle>Faça login na plataforma</DialogTitle>
+            <DialogDescription>
+              Conecte-se usando sua conta do Google.
+            </DialogDescription>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image alt="google" src="/google.svg" width={18} height={18} />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/*
         <Avatar className="border-2 border-solid border-primary">
           <AvatarImage src="https://static.vecteezy.com/system/resources/previews/030/712/176/large_2x/beautiful-samurai-woman-generative-ai-free-photo.jpg" />
         </Avatar>
@@ -21,6 +48,7 @@ const SidebarSheet = () => {
           <p className="font-bold">Pessoa</p>
           <p className="text-xs">pessoa@email.com</p>
         </div>
+        */}
       </div>
 
       <div className="py-5 flex flex-col gap-2 border-b border-solid">
@@ -56,7 +84,7 @@ const SidebarSheet = () => {
 
       <div className="py-5 flex flex-col gap-2">
         <Button className="gap-2 justify-start" variant="ghost">
-          <LogInIcon size={18} /> Sair da conta
+          <LogOutIcon size={18} /> Sair da conta
         </Button>
       </div>
     </SheetContent>
