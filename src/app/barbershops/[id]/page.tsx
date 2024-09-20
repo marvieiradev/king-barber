e2333317468cf40be2bb915d3373b74369887bc5
Marcos Vieira <marcosvieira482@gmail.com>
2024-09-20 18:17:40 -0300

    feat: ensure booking can be created

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -84,7 +84,11 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
         <h2 className="text-xs font-bold text-gray-400 uppercase">Serviços</h2>
         <div className="space-y-3">
           {barbershop.services.map((service) => (
-            <ServiceItem key={service.id} service={service} />
+            <ServiceItem
+              key={service.id}
+              barbershop={barbershop}
+              service={service}
+            />
           ))}
         </div>
       </div>
