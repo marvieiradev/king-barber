167c0dab3951a6ae7cdb43f592159b744bb0206d
Marcos Vieira <marcosvieira482@gmail.com>
2024-09-24 01:29:03 -0300

    refactor: add data functions to get bookings

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -1,58 +1,24 @@
 import { getServerSession } from "next-auth";
 import Header from "../_components/header";
-import { db } from "../_lib/prisma";
 import { authOptions } from "../_lib/auth";
 import { notFound } from "next/navigation";
 import BookingItem from "../_components/booking-item";
+import { getConfirmedBookings } from "../_data/get-confirmed-bookings";
+import { getConcludedBookings } from "../_data/get-concluded-bookings";
 
 const Bookings = async () => {
   const session = await getServerSession(authOptions);
   if (!session?.user) {
     return notFound();
   }
-  const confirmedBookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
-      date: {
-        gte: new Date(),
-      },
-    },
-    include: {
-      service: {
-        include: {
-          barbershop: true,
-        },
-      },
-    },
-    orderBy: {
-      date: "asc",
-    },
-  });
-
-  const concluedBookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
-      date: {
-        lt: new Date(),
-      },
-    },
-    include: {
-      service: {
-        include: {
-          barbershop: true,
-        },
-      },
-    },
-    orderBy: {
-      date: "asc",
-    },
-  });
+  const confirmedBookings = await getConfirmedBookings();
+  const concludedBookings = await getConcludedBookings();
   return (
     <>
       <Header />
       <div className="p-5 space-y-3">
         <h1 className="text-xl font-bold">Agendamentos</h1>
-        {confirmedBookings.length == 0 && concluedBookings.length == 0 && (
+        {confirmedBookings.length == 0 && concludedBookings.length == 0 && (
           <p className="text-gray-400 font-semibold">
             Você não tem agendamentos
           </p>
@@ -71,12 +37,12 @@ const Bookings = async () => {
           </>
         )}
 
-        {concluedBookings.length > 0 && (
+        {concludedBookings.length > 0 && (
           <>
             <h2 className="mt-6 mb-3 uppercase text-xs font-bold text-gray-400">
               Concluidos
             </h2>
-            {concluedBookings.map((booking) => (
+            {concludedBookings.map((booking) => (
               <BookingItem
                 key={booking.id}
                 booking={JSON.parse(JSON.stringify(booking))}
